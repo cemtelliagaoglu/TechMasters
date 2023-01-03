ec82d21bb2f492f097afcf8900db422b59d229b4
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-01-03 23:37:58 +0300

    create Homework1

---
+++ Homework1.playground/Contents.swift
@@ -0,0 +1,94 @@
+import Cocoa
+
+var city = "Istanbul"
+var country = "Turkey"
+var phone = 5551112233
+var postalCode = 34752
+var email = "xyz@gmail.com"
+var profession = "Environmental Engineer"
+print("City: \(city)")
+print("Country: \(country)")
+print("Phone: \(phone)")
+print("Postal Code: \(postalCode)")
+print("Email: \(email)")
+print("Profession: \(profession)")
+
+var stockAmount = 500
+var customerName = "Cem"
+var balance = 15254.54
+var birthday = "06.04.1998"
+var salary:Double = 20000
+var maritalStatus = "Single"
+var productComment = "Amazing Product!"
+var paymentDate = "17.01.2023"
+var payment: Double = 5499.99
+var orderAmount = 5
+var carModel = 2023
+var bookName = "The Lord of the Rings"
+var publishDate = "29/07/1954"
+var discountAmount: Double = 50 / 100
+print("Stock Amount: \(stockAmount)")
+print("Customer Name: \(customerName)")
+print("Balance: \(balance)")
+print("Birthday: \(birthday)")
+print("Salary: \(salary)")
+print("Marital Status: \(maritalStatus)")
+print("Product Comment: \(productComment)")
+print("Payment Date: \(paymentDate)")
+print("Payment: \(payment)")
+print("Order Amount: \(orderAmount)")
+print("Car Model: \(carModel)")
+print("Book Name: \(bookName)")
+print("Publish Date: \(publishDate)")
+print("Discount Amount: \(discountAmount)")
+
+
+var roomNumber = 3
+var latitude = 20
+var longitude = 32
+var productName = "Watch"
+var foodPrice = 49.90
+var brand = "Casio"
+var musicName = "Soldier of Fortune"
+var videoTime = 1884
+var productRating = "10/10"
+var imageName = "sea.jpg"
+var fileFormat = ".PDF"
+var color = "red"
+var colorCode = "A6BF54"
+var phoneModel = "iPhone 11"
+var screenSizeInInch = 6.1
+var weightInGrams = 200
+print("Room Number: \(roomNumber)")
+print("Latitude: \(latitude), Longitude:\(longitude)")
+print("Product Name: \(productName)")
+print("Food Price: \(foodPrice)")
+print("Brand: \(brand)")
+print("Music Name: \(musicName)")
+print("Video Time: \(videoTime)")
+print("Product Rating: \(productRating)")
+print("Image Name: \(imageName)")
+print("File Format: \(fileFormat)")
+print("Color: \(color), Color Code: \(colorCode)")
+print("Phone Model: \(phoneModel), Screen Size(Inch): \(screenSizeInInch)")
+print("Weight(g): \(weightInGrams)")
+
+var nationalDay = "April 23"
+var holiday = "August 30"
+var reservationDate = "23.05.2023"
+var streetName = "Ataturk Street"
+var busLine = "14B"
+var remainingMinutes = 30
+var followingCode = 123456789
+var couponTime = 5
+var couponCode = 123456
+var billAddress = "Ataturk St. 5/2"
+print("National Day: \(nationalDay)")
+print("Holiday: \(holiday)")
+print("Reservation Date: \(reservationDate)")
+print("Street Name: \(streetName)")
+print("Bus Line: \(busLine)")
+print("Remaining Minutes: \(remainingMinutes)")
+print("Following Code: \(followingCode)")
+print("Coupon Code: \(couponCode), Coupon Time: \(couponTime)")
+print("Bill Address: \(billAddress)")
